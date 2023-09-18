1b5687646d34c95d47ad0bb1744363368a45ee36
Donny van Eijk <donnyvaneijk873@gmail.com>
2023-09-18 10:28:47 +0200

    commit ala chnage

--- site/index.php
+++ site/index.php
@@ -1,15 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Side Hustle</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <!-- pascal php server is recommended (de extension) -->
-<h1>The Side Hustles</h1>
+    <div class="main">
+        <div class='header'>
+            <h1>The Side Hustles</h1>
+        </div>
+    </div>
+    <div class='new-l'>
+            <h2>New Releases</h2>
+        </div>
+
+     <div class='most-p'>
+            <h2>Most played games</h2>
+        </div>
 
 
 
 </body>
+
 </html>
